22fbb5c72a4c720b1075d51e950ab02c9aea492b
imemmul <emrulurak17@outlook.com>
2023-08-22 14:25:06 +0300

    stego arch

---
+++ segmentation/models/modules.py
@@ -0,0 +1,155 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from utils import *
+
+
+class ContrastiveCRFLoss(nn.Module):
+
+    def __init__(self, n_samples, alpha, beta, gamma, w1, w2, shift):
+        super(ContrastiveCRFLoss, self).__init__()
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.w1 = w1
+        self.w2 = w2
+        self.n_samples = n_samples
+        self.shift = shift
+
+    def forward(self, guidance, clusters):
+        device = clusters.device
+        assert (guidance.shape[0] == clusters.shape[0])
+        assert (guidance.shape[2:] == clusters.shape[2:])
+        h = guidance.shape[2]
+        w = guidance.shape[3]
+
+        coords = torch.cat([
+            torch.randint(0, h, size=[1, self.n_samples], device=device),
+            torch.randint(0, w, size=[1, self.n_samples], device=device)], 0)
+
+        selected_guidance = guidance[:, :, coords[0, :], coords[1, :]]
+        coord_diff = (coords.unsqueeze(-1) - coords.unsqueeze(1)).square().sum(0).unsqueeze(0)
+        guidance_diff = (selected_guidance.unsqueeze(-1) - selected_guidance.unsqueeze(2)).square().sum(1)
+
+        sim_kernel = self.w1 * torch.exp(- coord_diff / (2 * self.alpha) - guidance_diff / (2 * self.beta)) + \
+                     self.w2 * torch.exp(- coord_diff / (2 * self.gamma)) - self.shift
+
+        selected_clusters = clusters[:, :, coords[0, :], coords[1, :]]
+        cluster_sims = torch.einsum("nka,nkb->nab", selected_clusters, selected_clusters)
+        return -(cluster_sims * sim_kernel)
+
+
+class ContrastiveCorrelationLoss(nn.Module):
+
+    def __init__(self, cfg, ):
+        super(ContrastiveCorrelationLoss, self).__init__()
+        self.cfg = cfg
+
+    def standard_scale(self, t):
+        t1 = t - t.mean()
+        t2 = t1 / t1.std()
+        return t2
+
+    def helper(self, f1, f2, c1, c2, shift):
+        with torch.no_grad():
+            # Comes straight from backbone which is currently frozen. this saves mem.
+            fd = tensor_correlation(norm(f1), norm(f2))
+
+            if self.cfg.pointwise:
+                old_mean = fd.mean()
+                fd -= fd.mean([3, 4], keepdim=True)
+                fd = fd - fd.mean() + old_mean
+
+        cd = tensor_correlation(norm(c1), norm(c2))
+
+        if self.cfg.zero_clamp:
+            min_val = 0.0
+        else:
+            min_val = -9999.0
+
+        if self.cfg.stabalize:
+            loss = - cd.clamp(min_val, .8) * (fd - shift)
+        else:
+            loss = - cd.clamp(min_val) * (fd - shift)
+
+        return loss, cd
+
+    def forward(self,
+                orig_feats: torch.Tensor, orig_feats_pos: torch.Tensor,
+                orig_salience: torch.Tensor, orig_salience_pos: torch.Tensor,
+                orig_code: torch.Tensor, orig_code_pos: torch.Tensor,
+                ):
+
+        coord_shape = [orig_feats.shape[0], self.cfg.feature_samples, self.cfg.feature_samples, 2]
+
+        if self.cfg.use_salience:
+            coords1_nonzero = sample_nonzero_locations(orig_salience, coord_shape)
+            coords2_nonzero = sample_nonzero_locations(orig_salience_pos, coord_shape)
+            coords1_reg = torch.rand(coord_shape, device=orig_feats.device) * 2 - 1
+            coords2_reg = torch.rand(coord_shape, device=orig_feats.device) * 2 - 1
+            mask = (torch.rand(coord_shape[:-1], device=orig_feats.device) > .1).unsqueeze(-1).to(torch.float32)
+            coords1 = coords1_nonzero * mask + coords1_reg * (1 - mask)
+            coords2 = coords2_nonzero * mask + coords2_reg * (1 - mask)
+        else:
+            coords1 = torch.rand(coord_shape, device=orig_feats.device) * 2 - 1
+            coords2 = torch.rand(coord_shape, device=orig_feats.device) * 2 - 1
+
+        feats = sample(orig_feats, coords1)
+        code = sample(orig_code, coords1)
+
+        feats_pos = sample(orig_feats_pos, coords2)
+        code_pos = sample(orig_code_pos, coords2)
+
+        pos_intra_loss, pos_intra_cd = self.helper(
+            feats, feats, code, code, self.cfg.pos_intra_shift)
+        pos_inter_loss, pos_inter_cd = self.helper(
+            feats, feats_pos, code, code_pos, self.cfg.pos_inter_shift)
+
+        neg_losses = []
+        neg_cds = []
+        for i in range(self.cfg.neg_samples):
+            perm_neg = super_perm(orig_feats.shape[0], orig_feats.device)
+            feats_neg = sample(orig_feats[perm_neg], coords2)
+            code_neg = sample(orig_code[perm_neg], coords2)
+            neg_inter_loss, neg_inter_cd = self.helper(
+                feats, feats_neg, code, code_neg, self.cfg.neg_inter_shift)
+            neg_losses.append(neg_inter_loss)
+            neg_cds.append(neg_inter_cd)
+        neg_inter_loss = torch.cat(neg_losses, axis=0)
+        neg_inter_cd = torch.cat(neg_cds, axis=0)
+
+        return (pos_intra_loss.mean(),
+                pos_intra_cd,
+                pos_inter_loss.mean(),
+                pos_inter_cd,
+                neg_inter_loss,
+                neg_inter_cd)
+
+class ClusterLookup(nn.Module):
+
+    def __init__(self, dim: int, n_classes: int):
+        super(ClusterLookup, self).__init__()
+        self.n_classes = n_classes
+        self.dim = dim
+        self.clusters = torch.nn.Parameter(torch.randn(n_classes, dim))
+
+    def reset_parameters(self):
+        with torch.no_grad():
+            self.clusters.copy_(torch.randn(self.n_classes, self.dim))
+
+    def forward(self, x, alpha, log_probs=False):
+        normed_clusters = F.normalize(self.clusters, dim=1)
+        normed_features = F.normalize(x, dim=1)
+        inner_products = torch.einsum("bchw,nc->bnhw", normed_features, normed_clusters)
+
+        if alpha is None:
+            cluster_probs = F.one_hot(torch.argmax(inner_products, dim=1), self.clusters.shape[0]) \
+                .permute(0, 3, 1, 2).to(torch.float32)
+        else:
+            cluster_probs = nn.functional.softmax(inner_products * alpha, dim=1)
+
+        cluster_loss = -(cluster_probs * inner_products).sum(1).mean()
+        if log_probs:
+            return nn.functional.log_softmax(inner_products * alpha, dim=1)
+        else:
+            return cluster_loss, cluster_probs
---
+++ segmentation/train.py
@@ -0,0 +1,6 @@
+from models.stego import Stego
+import argsparse
+
+
+
+
